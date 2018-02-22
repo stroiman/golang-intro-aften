6531272212c7d2b26dd7867c71a5071ca8b8e775
Peter Strøiman <peter@stroiman.com>
2018-02-22 21:44:27 +0100

    Remove blog feature

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,9 +1,4 @@
 import { combineReducers } from 'redux';
-import blogs, * as fromBlogs from './features/blogs/reducers';
 
 export default combineReducers({
-  blogs
 });
-
-export const getBlogsLoadingState = state => fromBlogs.getLoadingState(state.blogs);
-
