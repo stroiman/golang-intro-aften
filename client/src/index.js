f1ba9d98479e336a716d672e38b54f33c497577c
Peter Strøiman <peter@stroiman.com>
2018-02-23 16:41:33 +0100

    Client loads messages during startup

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 import App from './App';
 import { unregister } from './registerServiceWorker';
 import { createStore } from './store';
+import * as actions from './ducks/messages/actions';
 
 const store = createStore();
+store.dispatch(actions.fetchMessages());
 
 ReactDOM.render(
   <Provider store={store}>
