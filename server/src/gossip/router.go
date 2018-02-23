b964aad697ea641d6f0e26efc1b94e199546d4af
Peter Strøiman <peter@stroiman.com>
2018-02-23 17:36:44 +0100

    Return two skeleton messages from /messages

--- server/src/gossip/router.go
+++ server/src/gossip/router.go
@@ -1,11 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func pong(wr http.ResponseWriter, req *http.Request) {
 	wr.Write([]byte("pong"))
 }
 
+func getMessages(wr http.ResponseWriter, req *http.Request) {
+	wr.Header().Set("Content-Type", "application/json")
+	wr.WriteHeader(http.StatusOK)
+	wr.Write([]byte("[{},{}]"))
+}
+
 func createRootHandler() http.Handler {
-	return http.HandlerFunc(pong)
+	router := mux.NewRouter()
+	router.HandleFunc("/ping", pong).Methods("get")
+	router.HandleFunc("/messages", getMessages).Methods("get")
+	return router
 }
---
+++ server/src/gossip/router_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Router", func() {
+	Describe("/messages", func() {
+		var (
+			recorder *httptest.ResponseRecorder
+		)
+
+		BeforeEach(func() {
+			recorder = httptest.NewRecorder()
+			request := httptest.NewRequest("GET", "/messages", nil)
+			handler := createRootHandler()
+			handler.ServeHTTP(recorder, request)
+		})
+
+		It("Returns HTTP 200", func() {
+			Expect(recorder.Code).To(Equal(200))
+		})
+
+		It("Returns Content-Type=application/json", func() {
+			actual := recorder.Header().Get("Content-Type")
+			Expect(actual).To(Equal("application/json"))
+		})
+
+		It("Has two objects", func() {
+			var result []interface{}
+			err := json.NewDecoder(recorder.Body).Decode(&result)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(HaveLen(2))
+		})
+	})
+})
