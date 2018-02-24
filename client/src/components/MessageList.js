512aad291fc8d7784ec7586c06439be7d9364e2d
Peter StrÃ¸iman <peter@stroiman.com>
2018-02-24 16:52:05 +0100

    Auto scroll to bottom

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -3,20 +3,39 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import * as getters from '../reducers';
 
-export const Message = ({message}) => (
-  <div className="card bg-light mb-3">
-    <div className="card-body" style={{padding: "0.5rem"}} >
-      {message.message}
-    </div>
-  </div>
-);
+export class Message extends React.Component {
+  render() {
+    const message = this.props.message;
+    return (
+      <div className="card bg-light mb-3">
+        <div className="card-body" style={{padding: "0.5rem"}} >
+          {message.message}
+        </div>
+      </div>
+    );
+  }
+};
 
 class MessageList extends React.Component {
+  scrollToBottom () {
+    this.messageEnd && this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   render() {
-    this.rendered = true;
     return (
       <div>
         { this.props.messages.map(x => <Message key={x.id} message={x} />) }
+        <div style={{ float:"left", clear: "both" }}
+          ref={(el) => { this.messagesEnd = el; }}>
+        </div>
       </div>);
   }
 }
