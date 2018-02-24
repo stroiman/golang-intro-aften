84442e52e1d3e418c342d040cee62e0e2ed9f78c
Peter Strøiman <peter@stroiman.com>
2018-02-24 13:56:41 +0100

    Fetch from port 9000 in production builds

--- client/src/ducks/api.js
+++ client/src/ducks/api.js
@@ -1,5 +1,14 @@
+import url from 'url';
+
+const getUrl = (relativeUrl) => {
+  switch (process.env.NODE_ENV) {
+    case "production": return url.resolve("http://127.0.0.1:9000/", relativeUrl);
+    case "development": return relativeUrl;
+    default: return relativeUrl;
+  }
+}
 export const get = async (url, body) => {
-  const response = await fetch(url, { body });
+  const response = await fetch(getUrl(url), { body });
   const ok = response.ok;
   let json = ok ? await response.json() : null;
   return {
