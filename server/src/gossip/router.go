c42862ee3b15f1cc52abbb0c923f9962925e2d1f
Peter StrÃ¸iman <peter@stroiman.com>
2018-02-24 10:33:27 +0100

    Return simple json struct from server

--- server/src/gossip/router.go
+++ server/src/gossip/router.go
@@ -1,19 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type Message struct {
+	Id      string `json:"id"`
+	Message string `json:"message"`
+}
+
+func GetMessages() []Message {
+	return []Message{{
+		Id:      "1",
+		Message: "Foo",
+	}, {
+		Id:      "2",
+		Message: "Bar",
+	}}
+}
 
 func pong(wr http.ResponseWriter, req *http.Request) {
 	wr.Write([]byte("pong"))
 }
 
 func getMessages(wr http.ResponseWriter, req *http.Request) {
-	wr.Header().Set("Content-Type", "application/json")
-	wr.WriteHeader(http.StatusOK)
-	wr.Write([]byte("[{},{}]"))
+	if response, err := json.Marshal(GetMessages()); err == nil {
+		wr.Header().Set("Content-Type", "application/json")
+		wr.WriteHeader(http.StatusOK)
+		wr.Write(response)
+	} else {
+		wr.WriteHeader(500)
+	}
 }
 
 func createRootHandler() http.Handler {
