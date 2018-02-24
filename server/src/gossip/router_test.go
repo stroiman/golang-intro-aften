7783c9bab1524c187b54f5b2111fa0e57c8a8ec1
Peter Strøiman <peter@stroiman.com>
2018-02-24 11:23:49 +0100

    Mock repository in route tests

--- server/src/gossip/router_test.go
+++ server/src/gossip/router_test.go
@@ -4,20 +4,38 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 
+	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"gossip/domain"
 )
+
+type MessageList []domain.Message
+
+func (m MessageList) GetMessages() []domain.Message { return m }
+
+func NewMessage() domain.Message {
+	return domain.Message{
+		Id: uuid.New().String(),
+	}
+}
 
 var _ = Describe("Router", func() {
 	Describe("/api/messages", func() {
 		var (
 			recorder *httptest.ResponseRecorder
+			messages []domain.Message
 		)
 
 		BeforeEach(func() {
 			recorder = httptest.NewRecorder()
+			messages = make([]domain.Message, 0)
+		})
+
+		JustBeforeEach(func() {
 			request := httptest.NewRequest("GET", "/api/messages", nil)
-			handler := createRootHandler()
+			handler := NewMessageHandler(MessageList(messages))
 			handler.ServeHTTP(recorder, request)
 		})
 
@@ -30,11 +48,16 @@ var _ = Describe("Router", func() {
 			Expect(actual).To(Equal("application/json"))
 		})
 
-		It("Has two objects", func() {
-			var result []interface{}
-			err := json.NewDecoder(recorder.Body).Decode(&result)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(result).To(HaveLen(2))
+		Context("Repository has two messages", func() {
+			BeforeEach(func() {
+				messages = append(messages, NewMessage(), NewMessage())
+			})
+			It("Has two objects", func() {
+				var result []interface{}
+				err := json.NewDecoder(recorder.Body).Decode(&result)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(HaveLen(2))
+			})
 		})
 	})
 })
