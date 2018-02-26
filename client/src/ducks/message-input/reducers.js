d739f1739a25950052a750fce56222cea7df4622
Peter Strøiman <peter@stroiman.com>
2018-02-26 18:31:26 +0100

    Move message input to new property
    Allows us to add more properties

--- client/src/ducks/message-input/reducers.js
+++ client/src/ducks/message-input/reducers.js
@@ -1,4 +1,6 @@
-export default (state = "", action) => {
+import { combineReducers } from 'redux';
+
+const input = (state = "", action) => {
   switch (action.type) {
     case "MESSAGE_INPUT_SET": return action.payload;
     case "MESSAGE_POSTED": return "";
@@ -6,4 +8,8 @@ export default (state = "", action) => {
   }
 };
 
-export const getInput = state => state;
+export default combineReducers({
+  input
+})
+
+export const getInput = state => state.input;
