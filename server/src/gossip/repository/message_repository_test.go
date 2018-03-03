a37bedcb55165c3307d00837411d89d7b2f6f240
Peter Strøiman <peter@stroiman.com>
2018-03-03 12:11:25 +0100

    Created skeleton specs for repository

---
+++ server/src/gossip/repository/message_repository_test.go
@@ -0,0 +1,12 @@
+package repository_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	//. "github.com/onsi/gomega"
+	//. "gossip/repository"
+)
+
+var _ = Describe("MessageRepository", func() {
+	It("Has a dummy spec", func() {
+	})
+})
