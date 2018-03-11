7c1fd2544cabadf33b5e82300933286ca49f43c0
Peter Strøiman <peter@stroiman.com>
2018-03-11 14:56:20 +0100

    Fetch messages from server on startup

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,14 @@ import App from './App';
 import { unregister } from './registerServiceWorker';
 import { createStore } from './store';
 import * as actions from './ducks/auth/actions';
+import * as messageActions from './ducks/messages/actions';
 import * as pollActions from './ducks/polling/actions';
 
 const store = createStore({logging: true});
 if (process.env.NODE_ENV === "development") {
   store.dispatch(actions.loginUser({username: 'stroiman'}));
   store.dispatch(pollActions.startWebSocket());
+  store.dispatch(messageActions.fetchMessages());
 }
 
 ReactDOM.render(
